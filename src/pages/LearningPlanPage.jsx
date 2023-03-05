9e32b65118c911715aff32c37245f7c301e3ec9a
Phillip Friis-Alstrup <phfr38643@edu.ucl.dk>
2023-03-06 00:17:48 +0100

    LearningPlan for week 10 + 11

--- src/pages/LearningPlanPage.jsx
+++ src/pages/LearningPlanPage.jsx
@@ -1,4 +1,5 @@
 import { Card, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import PageHeader from "../components/PageHeader";
 
 export default function LearningPlanPage() {
@@ -10,11 +11,117 @@ export default function LearningPlanPage() {
         <div className="learningplan-cards">
           <Card>
             <Card.Body>
-              <h2>Uge 8 + 9</h2>
+              <h2>Uge 10 + 11</h2>
               <div className="react-goals">
                 <h3>React</h3>
                 <div className="table-responsive">
                   <table class="table table-bordered" style={{ width: "100%" }}>
+                    <thead>
+                      <tr>
+                        <th scope="col" style={{ width: "25%" }}>
+                          Delmål
+                        </th>
+                        <th scope="col" style={{ width: "25%" }}>
+                          Hvordan vil jeg opnå det?
+                        </th>
+                        <th scope="col" style={{ width: "25%" }}>
+                          Hvornår har jeg opnået det?
+                        </th>
+                        <th scope="col" style={{ width: "25%" }}>
+                          Evaluering
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          Forstå opsætning af globalstate i en React-applikation
+                          og viden om hvordan det kan håndteres ved hjælp af et
+                          eksternt library.
+                        </td>
+                        <td>
+                          Jeg vil undersøge hvordan man kan håndtere globalstate
+                          i en React appliktion ved at implementere det i mit
+                          elpris-produkt.
+                        </td>
+                        <td>
+                          Når jeg har et fungerende eksempel på brug/ændring af
+                          globalstate i mit elpris-produkt, så der ikke behøves
+                          fetching af data, ved navigering rund på siden.
+                        </td>
+                        <td>Kommer.</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <div className="testing-goals">
+                <h3>Testing</h3>
+                <div className="table-responsive">
+                  <table class="table table-bordered" style={{ width: "100%" }}>
+                    <thead>
+                      <tr>
+                        <th scope="col" style={{ width: "25%" }}>
+                          Delmål
+                        </th>
+                        <th scope="col" style={{ width: "25%" }}>
+                          Hvordan vil jeg opnå det?
+                        </th>
+                        <th scope="col" style={{ width: "25%" }}>
+                          Hvornår har jeg opnået det?
+                        </th>
+                        <th scope="col" style={{ width: "25%" }}>
+                          Evaluering
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          Forståelse for forskellige typer af frontend testing
+                          af React.
+                        </td>
+                        <td>
+                          Jeg vil undersøge hvordan man kan teste en
+                          React-applikation, ved bla. at kigge på accessibility-
+                          og e2e-test.
+                        </td>
+                        <td>
+                          Når jeg har en grundlæggende forståelse for målet,
+                          samt en plan for udførelse af accessibility- og
+                          e2e-test.
+                        </td>
+                        <td>Kommer.</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          Forståelse og viden om hvordan man udfører e2e-test via et test-framework kaldet Cypress.
+                        </td>
+                        <td>
+                          Jeg vil undersøge hvordan man kan udføre e2e-test i Cypress og udføre en e2e-test af mit elpris-produkt.
+                        </td>
+                        <td>
+                          Når jeg har forståelse for brug af Cypress og en fungerende test af mit elpris-produkt.
+                        </td>
+                        <td>Kommer.</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Body>
+              <h2>Uge 8 + 9</h2>
+              <div className="react-goals">
+                <h3>React</h3>
+                <div className="table-responsive">
+                  <table
+                    className="table table-bordered"
+                    style={{ width: "100%" }}
+                  >
                     <thead>
                       <tr>
                         <th scope="col" style={{ width: "25%" }}>
@@ -46,7 +153,14 @@ export default function LearningPlanPage() {
                           Når jeg har et fungerende side, og kan forklare
                           hvordan state management fungere i React.
                         </td>
-                        <td>Kommer.</td>
+                        <td>
+                          Jeg har opnået forståelse og viden indenfor state
+                          management og hooks, igennem mit første produkt med
+                          React. Jeg har nu en fungerende side:{" "}
+                          <Link to="https://elpriser.phillipf.dk">
+                            elpriser.phillipf.dk
+                          </Link>
+                        </td>
                       </tr>
                       <tr>
                         <td>
@@ -61,7 +175,14 @@ export default function LearningPlanPage() {
                           Når jeg har en side der henter data fra en ekstern
                           api, og at jeg kan forklare hvordan det virker.
                         </td>
-                        <td>Kommer.</td>
+                        <td>
+                          Jeg har opnået forståelse og viden indenfor fetching
+                          af data, igennem mit første produkt med React. Jeg har
+                          nu en fungerende side:{" "}
+                          <Link to="https://elpriser.phillipf.dk">
+                            elpriser.phillipf.dk
+                          </Link>
+                        </td>
                       </tr>
                     </tbody>
                   </table>
@@ -70,7 +191,10 @@ export default function LearningPlanPage() {
               <div className="testing-goals">
                 <h3>Testing</h3>
                 <div className="table-responsive">
-                  <table class="table table-bordered" style={{ width: "100%" }}>
+                  <table
+                    className="table table-bordered"
+                    style={{ width: "100%" }}
+                  >
                     <thead>
                       <tr>
                         <th scope="col" style={{ width: "25%" }}>
@@ -103,7 +227,10 @@ export default function LearningPlanPage() {
                           samt en plan for udførelse af accessibility- og
                           e2e-test.
                         </td>
-                        <td>Kommer.</td>
+                        <td>
+                          Målet er ikke nået indenfor tidsrammen. Derfor tager
+                          jeg det med i næste læringsplan.
+                        </td>
                       </tr>
                     </tbody>
                   </table>
@@ -118,7 +245,10 @@ export default function LearningPlanPage() {
               <div className="react-goals">
                 <h3>React</h3>
                 <div className="table-responsive">
-                  <table class="table table-bordered" style={{ width: "100%" }}>
+                  <table
+                    className="table table-bordered"
+                    style={{ width: "100%" }}
+                  >
                     <thead>
                       <tr>
                         <th scope="col" style={{ width: "25%" }}>
@@ -163,7 +293,10 @@ export default function LearningPlanPage() {
               <div className="testing-goals">
                 <h3>Testing</h3>
                 <div className="table-responsive">
-                  <table class="table table-bordered" style={{ width: "100%" }}>
+                  <table
+                    className="table table-bordered"
+                    style={{ width: "100%" }}
+                  >
                     <thead>
                       <tr>
                         <th scope="col" style={{ width: "25%" }}>
